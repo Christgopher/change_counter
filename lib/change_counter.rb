70053489602cc1b4e74c824420fb4cdac2f105bf
Chris LeCompte & Jessi Iler <student@epicodus.com>
2015-04-23 10:22:21 -0700

    Finished counter method

--- lib/change_counter.rb
+++ lib/change_counter.rb
@@ -3,10 +3,37 @@ class Float
   define_method(:change_counter) do
     numb = self
     quarter_counter = 0
+    dime_counter = 0
+    nickel_counter = 0
+    penny_counter = 0
+    coin_array = []
     until numb.<(0.25)
       numb = numb.-(0.25)
       quarter_counter = quarter_counter.+(1)
     end
-    quarter_counter
+    until numb.<(0.10)
+      numb = numb.-(0.10)
+      dime_counter = dime_counter.+(1)
+    end
+    until numb.<(0.05)
+      numb = numb.-(0.05)
+      nickel_counter = nickel_counter.+(1)
+    end
+    until numb.<(0.01)
+      numb = numb.-(0.01)
+      penny_counter = penny_counter.+(1)
+    end
+    penny_counter = penny_counter.+(1)
+    coin_array.push("You have ")
+    coin_array.push(quarter_counter)
+    coin_array.push(" quarters, ")
+    coin_array.push(dime_counter)
+    coin_array.push(" dimes, ")
+    coin_array.push(nickel_counter)
+    coin_array.push(" nickels, and ")
+    coin_array.push(penny_counter)
+    coin_array.push(" pennies. You're rich!!")
+    result_array = coin_array.join()
+    result_array
 end
 end
--- spec/change_counter_spec.rb
+++ spec/change_counter_spec.rb
@@ -3,8 +3,12 @@ require('change_counter')
 
 describe('Float#change_counter') do
 
-  it('should count possible quarters in number') do
-    expect((0.97).change_counter).to(eq(3))
+#  it('should count possible quarters in number') do
+#    expect((0.97).change_counter).to(eq(3))
 
-  end
+#  end
+
+  it('should print out the change') do
+    expect((0.97).change_counter).to(eq("You have 3 quarters, 2 dimes, 0 nickels, and 2 pennies. You're rich!!"))
+end
 end
