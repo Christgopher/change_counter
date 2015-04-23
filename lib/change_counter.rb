7e5e1fbff29f98c764839b4c60d508ef14d271e2
Chris LeCompte & Jessi Iler <student@epicodus.com>
2015-04-23 09:52:38 -0700

    Now counts quarters

--- lib/change_counter.rb
+++ lib/change_counter.rb
@@ -1,7 +1,12 @@
 class Float
 
   define_method(:change_counter) do
-    #Code goes here
-
+    numb = self
+    quarter_counter = 0
+    until numb.<(0.25)
+      numb = numb.-(0.25)
+      quarter_counter = quarter_counter.+(1)
+    end
+    quarter_counter
 end
 end
